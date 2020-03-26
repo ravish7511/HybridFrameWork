0d79eae483d2e50fc69513371d6f675dedfc8ee3
Ravisha <ravishacr@gmail.com>
2020-03-26 19:02:19 +0530

    push

---
+++ src/test/java/managers/PageObjectManager.java
@@ -0,0 +1,27 @@
+package managers;
+
+import org.openqa.selenium.WebDriver;
+import pom.EnterTimeTrack;
+import pom.Login_Page;
+
+public class PageObjectManager {
+
+	private WebDriver driver;
+	private Login_Page login_page;
+	private EnterTimeTrack entertimetrack_page;
+
+	public PageObjectManager(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public Login_Page getLoginPage() {
+		return (login_page == null) ? login_page = new Login_Page(driver)
+				: login_page;
+	}
+
+	public EnterTimeTrack getEntertimetrack_page() {
+		return (entertimetrack_page == null) ? entertimetrack_page = new EnterTimeTrack(
+				driver) : entertimetrack_page;
+	}
+
+}
